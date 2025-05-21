a9de87ca87c5e6fb6a0357fbbe36a1b8bde02ba7
bimatakutkecoa <bimanaderli@gmail.com>
2025-05-21 21:52:37 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("welcome to the web")
 st.write(
-    "terimakasih sudah masuk."
+    "terimakasih sudah masuk.")
 st.title("Kosambi day")
 st.write(
     "jadi hari ini aku nganterin kylie asyifa rizky ke kosambi buat ngeciin baju batik dupul gaiss, dan ternyata ngecilin bantiknya bisa ditunggu sekitar 20 menitan, tapiii...karena kita gatau mau ngapain jai aku ajak di jalan jalan keliling bandung gais...SERU BANGET.")
