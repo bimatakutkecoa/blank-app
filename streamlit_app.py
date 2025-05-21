fbfaecbb4c36455fb41b33ad442952312a64f8c6
bimatakutkecoa <bimanaderli@gmail.com>
2025-05-21 21:45:00 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
-import streamlit as st
-
-st.title("🎈 My new app")
+st.title("welcome to the web")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Halooo, terimaksih sudah masuk."
 )
+st.title("Kosambi day")
+st.write(
+    "jadi hari ini aku nganterin kylie asyifa rizky ke kosambi buat ngeciin baju batik dupul gaiss, dan ternyata ngecilin bantiknya bisa ditunggu sekitar 20 menitan, tapiii...karena kita gatau mau ngapain jai aku ajak di jalan jalan keliling bandung gais...SERU BANGET.")
+st.image("IMG_20250424_190909_919.jpg", width=200)
