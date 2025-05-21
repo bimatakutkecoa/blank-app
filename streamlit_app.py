94dc2f1e989b9ca924396b38216d2ba203047384
bimatakutkecoa <bimanaderli@gmail.com>
2025-05-21 21:58:27 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,4 @@ st.write(
 st.title("Kosambi day")
 st.write(
     "jadi hari ini aku nganterin kylie asyifa rizky ke kosambi buat ngeciin baju batik dupul gaiss, dan ternyata ngecilin bantiknya bisa ditunggu sekitar 20 menitan, tapiii...karena kita gatau mau ngapain jai aku ajak di jalan jalan keliling bandung gais...SERU BANGET.")
-st.image("IMG_20250424_190909_919.jpg", width=200)
+st.image("IMG_20250424_191055_270.jpg", width=200
