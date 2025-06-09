03a22e55b1fb73d1cf27168b47a774496958535d
bimatakutkecoa <bimanaderli@gmail.com>
2025-06-09 09:52:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,47 @@ st.title("kylie lagi p5 senam")
 st.write("fyi aku liatin dia dari pagi sampe siang di kantin, dan ternyata si buce foto sama kili dan di kirim ke aku gais, rezeki nomplok ini mah")
 st.image("z0dxbb.jpg", width=200)
 st.write("masyaallah cantik banget sii")
+st.write("\n")
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title></title>
+
+<link href="favicon.ico" rel="shortcut icon" class="icon-love" type="images/x-ico">
+
+<link rel="stylesheet" href="css/love.css">
+
+
+</head>
+<body>
+
+    <h1>love 丽丽，永远在一起</h1>
+
+
+<div class="container" onselectstart="return false;" unselectable="on" style="-moz-user-select:none;">
+    <div class="body_left">
+        <img src="images/biubiubiu.gif" alt="biubiu love lili " ondragstart='return false;'>
+    </div>
+
+    <div class="body_center love">
+        <div class="block">
+            <div class="div1"></div>
+            <div class="div2"></div>
+            <div class="div3"></div>
+            <div class="div4"></div>
+        </div>
+    </div>
+</div>
+
+<div class="footer">
+    <div class="border">
+        <div class="border-top"></div>
+        <div class="border-bottom"></div>
+    </div>
+
+</div>
+<script type="text/javascript" src="js/love.js"></script>
+</body>
+</html>
